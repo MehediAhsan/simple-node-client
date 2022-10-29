cfd4da5310e9b432c2cde9664d57fce6fa6f083c
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-10-29 21:44:57 +0600

    simple node client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[users, setUsers] = useState([]);
+
+  useEffect( () => {
+    fetch('http://localhost:5000/users')
+    .then( res => res.json())
+    .then( data => setUsers(data))
+  }, [])
+
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email =form.email.value;
+    const user = {name, email};
+    console.log(user);
+    form.reset();
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+    .then( res => res.json())
+    .then( data => {
+      console.log(data);
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+    })
+    .catch( error => console.log(error))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name='name' placeholder='Name' />
+        <br />
+        <input type="email" name="email" placeholder='Email' />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
+
+      <h1>Total Users:{users.length}</h1>
+      <div>
+        {
+          users.map( user => <p key={user._id}>{user.name} {user.email}</p>)
+        }
+      </div>
     </div>
   );
 }
